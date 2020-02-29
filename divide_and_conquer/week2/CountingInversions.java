23675a3c2d78dc3a3562b1bc1ef53e88d9ed59e7
haikubabe <sreech94@gmail.com>
2020-02-29 22:18:17 +0530

    Modified this file

--- divide_and_conquer/week2/CountingInversions.java
+++ divide_and_conquer/week2/CountingInversions.java
@@ -1,8 +1,12 @@
 package divide_and_conquer.week2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CountingInversions {
 
-    private static int merge_and_countSplitInv(int[] A, int p, int q, int r) {
+    private static long merge_and_countSplitInv(int[] A, int p, int q, int r) {
         int nL = q - p + 1;
         int nR = r - q;
         int[] L = new int[nL];
@@ -16,7 +20,7 @@ public class CountingInversions {
         }
 
         int i=0,j=0,k=p;
-        int count_inversions = 0;
+        long count_inversions = 0;
 
         while (i<nL && j<nR) {
             if (L[i]<=R[j]) {
@@ -42,21 +46,32 @@ public class CountingInversions {
         return count_inversions;
     }
 
-    private static int sort_and_count(int[] A, int p, int r) {
+    private static long sort_and_count(int[] A, int p, int r) {
         if (p<r) {
             int q = (p+r)/2;
-            int x = sort_and_count(A, p, q);    //left inversion
-            int y = sort_and_count(A,q+1,r);    //right inversion
-            int z = merge_and_countSplitInv(A,p,q,r);   //split inversion
+            long x = sort_and_count(A, p, q);    //left inversion
+            long y = sort_and_count(A,q+1,r);    //right inversion
+            long z = merge_and_countSplitInv(A,p,q,r);   //split inversion
             return x+y+z;
         }
         return 0;
     }
 
     public static void main(String[] args)
+            throws IOException
     {
-        int[] A = {1,3,5,2,4,6};
+        /*int[] A = {1,3,5,2,4,6};
         int n = A.length;
-        System.out.println(sort_and_count(A,0,n-1));
+        System.out.println(sort_and_count(A,0,n-1));*/
+        int[] A = new int[100000];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        int i=0;
+        while ((line = bufferedReader.readLine()) != null) {
+            int n = Integer.parseInt(line.split("\\s+")[0]);
+            A[i] = n;
+            i++;
+        }
+        System.out.println(sort_and_count(A, 0, A.length-1));
     }
 }
